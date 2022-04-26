2ed3a56ff532de227d4a228488d7c3b82606d725
JgS0615 <jstasinos9@gmail.com>
2022-04-26 15:52:51 -0400

    updated (and pretty much finished) the main screen.

--- app/src/main/java/com/jonesclass/huangstasinos/crossmind/MainActivity.java
+++ app/src/main/java/com/jonesclass/huangstasinos/crossmind/MainActivity.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
