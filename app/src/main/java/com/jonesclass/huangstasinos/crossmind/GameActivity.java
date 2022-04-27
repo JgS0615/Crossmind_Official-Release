da5e65945865fa2c526181d07fac9f9bfeb897ca
JgS0615 <93215692+JgS0615@users.noreply.github.com>
2022-04-27 00:46:12 -0400

    Got team choosing to work
    I am not capable of testing these fixes. hope it works

--- app/src/main/java/com/jonesclass/huangstasinos/crossmind/GameActivity.java
+++ app/src/main/java/com/jonesclass/huangstasinos/crossmind/GameActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,15 +17,17 @@ public class GameActivity extends AppCompatActivity {
 
     public Random rNumber = new Random();
 
-    public int turn;
+    public String[] teams = new String[4];
+    public int turnCounter = 0;
     public int points;
     public boolean twoPlayers;
     public String teamChoice;
     public String teamChoice2;
+    public String color, color2;
     public ImageButton[][] imageButtons = new ImageButton[5][5];
     public boolean[][] selected = new boolean[5][5];
     public int[][] values = new int[5][5];
-
+    
 
     final String TAG = "GameActivityTag";
 
@@ -31,18 +36,108 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        teams = ["BioTeam","Knights","Outlanders","Techno"];
+        
         Log.d(TAG, "onCreate: Created");
         Intent newIntent = getIntent();
 
         twoPlayers = newIntent.getBooleanExtra("com.jonesclass.stasinos.ticTacSmack.TWO_PLAYERS", true);
         teamChoice = newIntent.getStringExtra("com.jonesclass.stasinos.ticTacSmack.TEAM_CHOICE_PLAYER_ONE");
         teamChoice2 = newIntent.getStringExtra("com.jonesclass.stasinos.ticTacSmack.TEAM_CHOICE_PLAYER_TWO");
+        
+        switch(teamChoice2) { // "BioTeam","Knights","Outlanders","Techno" //P1 (singlePlayer, and P2 for 2 Players)
+            case "BioTeam":
+                color = "green";
+                continue;
+            case "Knights":
+                color = "";
+                continue;
+            case "Outlanders":
+                color = "red";
+                continue;
+            case "Techno":
+                color = "blue";
+                continue;
+            default:
+                color = null; // this should NOT happen
+        }
+        if (twoPlayers) {
+            switch(teamChoice) {
+                case "BioTeam":
+                    color2 = "green";
+                    continue;
+                case "Knights":
+                    color2 = "black";
+                    continue;
+                case "Outlanders":
+                    color2 = "red";
+                    continue;
+                case "Techno":
+                    color2 = "blue";
+                    continue;
+                default:
+                    color2 = null; // this should NOT happen
+            }
+        } else {
+            List<String> teamList = Arrays.asList(teams);
+            Collections.shuffle(teamList);
+            teamList.toArray(teams);
+            teamChoice = teams.get(0);
+            switch(teamChoice) {
+                case "BioTeam":
+                    color2 = "green";
+                    continue;
+                case "Knights":
+                    color2 = "black";
+                    continue;
+                case "Outlanders":
+                    color2 = "red";
+                    continue;
+                case "Techno":
+                    color2 = "blue";
+                    continue;
+                default:
+                    color2 = null; // this should NOT happen
+            }
+        }
 
         if (twoPlayers) {
             Board board = new Board(teamChoice, teamChoice2);
         } else {
             Board board = new Board(teamChoice2);
         }
+        
+        for (int i = 0; i < imageButtons.length; i ++) {
+            for (int j = 0; j < imageButtons[].length; j ++) {
+                String buttonID = "tile_" + (i + 1) + "." + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                imageButtons[i][j] = ((ImageButton) findViewById(resID));
+                int finalI = i;
+                int finalJ = j;
+                imageButtons[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (turnCounter % 2 == 0) {
+                            // TODO: player1 turn
+                            String nameOfFile = "piece" + color;
+                            int photoResID = getResources().getDrawable();
+                            imageButtons[finalI][finalJ].setImageDrawable(getDrawable(res.getIdentifier(nameOfFile,"drawable",context.getPackageName())));
+                            turnCounter ++;
+                            player1Label.setVisibility(View.VISIBLE);
+                            player2Label.setVisibility(View.INVISIBLE);
+                        } else if(turnCounter % 2 == 1) {
+                            //TODO: player2 turn
+                            String nameOfFile = "piece" + color2;
+                            int photoResID = getResources().getDrawable();
+                            imageButtons[finalI][finalJ].setImageDrawable(getDrawable(res.getIdentifier(nameOfFile,"drawable",context.getPackageName())));
+                            player1Label.setVisibility(View.INVISIBLE);
+                            player2Label.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
+            }
+        }
+        
     }
 
 }
