b1189d2ed793aa3800bc8b46ad76a0cf300ba3be
Sunny <kingsunnyvhuang@gmail.com>
2022-04-28 14:24:37 -0400

    battle stuff

--- app/src/main/java/com/jonesclass/huangstasinos/crossmind/InstructionActivity.java
+++ app/src/main/java/com/jonesclass/huangstasinos/crossmind/InstructionActivity.java
@@ -1,11 +1,13 @@
 package com.jonesclass.huangstasinos.crossmind;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class InstructionActivity extends AppCompatActivity {
 
@@ -13,6 +15,13 @@ public class InstructionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
 
         Button button = findViewById(R.id.button_back);
         button.setOnClickListener(new View.OnClickListener() {
